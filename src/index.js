1951be4f66c996920a33eab570fe47f06a9c9f6c
Christian Samson <christiansamson@Christians-MacBook-Pro.local>
2023-10-28 22:07:44 -0400

    Update: added the iteration for menu bar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-const add = ""
+const menuBarNode = document.querySelector(".menu");
+const menuLinks = [
+  { text: "menu", class: "label" },
+  { text: "", class: "spacer" },
+  { text: "treatments", class: "item" },
+  { text: "fitness", class: "item" },
+  { text: "book now", class: "item" },
+];
+//Create <div> elements for the menu bar
+for (let i = 0; i < menuLinks.length; i++) {
+  const menuDivEl = document.createElement("div");
+  menuDivEl.classList.add(menuLinks[i].class);
+  const menuSpanEl = document.createElement("span");
+  menuSpanEl.textContent = menuLinks[i].text;
+  menuDivEl.appendChild(menuSpanEl);
+  menuBarNode.appendChild(menuDivEl);
+}
