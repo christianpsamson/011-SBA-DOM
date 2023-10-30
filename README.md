c91e2b9f51ded592472c738870cd8213f837393d
Christian Samson <85580709+christianpsamson@users.noreply.github.com>
2023-10-29 20:13:16 -0400

    Update: Added README file

---
+++ README.md
@@ -0,0 +1,20 @@
+SBA 316 - DOM MANIPULATION
+
+==========================================
+REQUIREMENTS INCLUDED:
+
+- selectElementByID: event handler 'handlingContactForm'
+- querySelector:
+- parent-child sibling relationship:
+- iterate through elements: menu navigation bar
+- createElement: inline error handling in contact form; fields cannot be empty
+- appendChild: menu navigation bar
+- textContent: inline error handling in contact form; fields cannot be empty
+- classList: inline error handling in contact form; style of error message is in Style.css, .error
+- attribute modification:
+- two event listeners: contact form
+- two BOM properties:
+- HTML form validation: included in the contact.html
+- event-based form validation: included in event handler 'handlingContactForm'
+- program runs without error
+``
--- src/index.js
+++ src/index.js
@@ -1,22 +1,63 @@
-const menuBarNode = document.querySelector(".menu");
-const menuLinks = [
-  { text: "meow-nu", class: "label" },
-  { text: "", class: "spacer" },
-  { text: "treatments", class: "item" },
-  { text: "fitness", class: "item" },
-  { text: "book now", class: "item" },
-];
-//Create <div> elements for the menu bar
-for (let i = 0; i < menuLinks.length; i++) {
-  const menuDivEl = document.createElement("div");
-  menuDivEl.classList.add(menuLinks[i].class);
-  const menuSpanEl = document.createElement("span");
-  menuSpanEl.textContent = menuLinks[i].text;
-  menuDivEl.appendChild(menuSpanEl);
-  menuBarNode.appendChild(menuDivEl);
-}
+// const menuBarNode = document.querySelector(".menu");
+// const menuLinks = [
+//   { text: "meow-nu", class: "label" },
+//   { text: "", class: "spacer" },
+//   { text: "treatments", class: "item" },
+//   { text: "fitness", class: "item" },
+//   { text: "book now", class: "item" },
+// ];
+// //Create <div> elements for the menu bar
+// for (let i = 0; i < menuLinks.length; i++) {
+//   const menuDivEl = document.createElement("div");
+//   menuDivEl.classList.add(menuLinks[i].class);
+//   const menuSpanEl = document.createElement("span");
+//   menuSpanEl.textContent = menuLinks[i].text;
+//   menuDivEl.appendChild(menuSpanEl);
+//   menuBarNode.appendChild(menuDivEl);
+// }
 
-// ************************************************************************ //
-// EVENT HANDLER FOR CONTACT PAGE
-// ************************************************************************ //
-const 
+//=====================================================================//
+// Event handler for contact page
+//=====================================================================//
+const handlingContactForm = (event) => {
+  event.preventDefault();
+  let formName = document.getElementById("name");
+  let formNameErr = document.getElementById("nameErr");
+  let formEmail = document.getElementById("email");
+  let formEmailErr = document.getElementById("emailErr");
+  let formNum = document.getElementById("number");
+  let formNumErr = document.getElementById("numErr");
+  let formMsg = document.getElementById("message");
+  let formMsgErr = document.getElementById("msgErr");
+
+  // If any of the input details is empty, display an inline error message
+  if (formName.value === "") {
+    formNameErr.classList.add("error");
+    formNameErr.textContent = "Please enter a valid name.";
+    formNameErr.style.display = "block";
+  }
+
+  if (formEmail.value === "") {
+    formEmailErr.classList.add("error");
+    formEmailErr.textContent = "Please enter a valid email address.";
+    formEmailErr.style.display = "block";
+  }
+
+  if (formNum.value === "") {
+    formNumErr.classList.add("error");
+    formNumErr.textContent = "Please enter a valid phone number.";
+    formNumErr.style.display = "block";
+  }
+
+  if (formMsg.value === "") {
+    formMsgErr.classList.add("error");
+    formMsgErr.textContent =
+      "Please enter a brief message about your reason to contact us.";
+    formMsgErr.style.display = "block";
+  }
+};
+//=====================================================================//
+// Register Event Handler for contact page
+//=====================================================================//
+let formEl = document.getElementById("form-input");
+formEl.addEventListener("submit", handlingContactForm);
